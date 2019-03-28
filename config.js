9ffdd0d2bee12603fc24ff8778061220b621c47c
Marcel <fleuren.marcel@gmail.com>
2019-03-28 16:02:29 +0100

    seventh attempt

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ENV: process.env.NODE_ENV || 'production',
-    PORT: process.env.PORT || 3000,
-    URL: process.env.BASE_URL || 'http://localhost:3000',
-    MONGODB_URI: process.env.MONGO_URI || 'mongodb://teacher:teacher123@ds119996.mlab.com:19996/live_poll'
-}
